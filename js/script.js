7a51dbfb3cb2823b91a3d916e16d792a0c9d1eb8
Kerchik <olainerul@inbox.ru>
2020-01-11 18:37:47 +0200

    2) Finished template, added sticky menu(without styles yet)

--- js/script.js
+++ js/script.js
@@ -7,3 +7,36 @@ let makeActive = (e) => {
     let target = e.target;
     target.classList.add("active");
 }
+let makeActivePhoto = (e) => {
+    let menu = document.getElementById("peopleSaySlider");
+    let children = menu.children;
+    for (let i=0;i<children.length;i++) {
+        children[i].classList.remove("activeImg")
+    }
+    let target = e.target;
+    target.classList.add("activeImg");
+
+    
+    changePeopleSayText(e);
+
+}
+let changePeopleSayText = (e) => {
+    let name = document.getElementById("name");
+    let job = document.getElementById("job");
+    name.innerHTML = e.target.getAttribute("name")
+    job.innerHTML = e.target.getAttribute("job")
+}
+(function onScroll() {
+    window.onscroll = function() {myFunction()};
+
+    let menu = document.body.getElementsByClassName("menu")[0];
+    let sticky = menu.offsetTop;
+
+    function myFunction() {
+    if (window.pageYOffset > sticky) {
+        menu.classList.add("sticky")
+    } else {
+        menu.classList.remove("sticky");
+    }
+    }
+})()
