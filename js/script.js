d7d7da0a97a8bf69c26f11f1d0d4d6a70d8d46ee
Kerchik <olainerul@inbox.ru>
2020-01-12 14:53:15 +0200

    4) added left/right button functionality in peopleSay slider; added styles to menu

--- js/script.js
+++ js/script.js
@@ -13,8 +13,7 @@ let makeActivePhoto = (e) => {
     for (let i=0;i<children.length;i++) {
         children[i].classList.remove("activeImg")
     }
-    let target = e.target;
-    target.classList.add("activeImg");
+    e.classList.add("activeImg");
 
     
     changePeopleSayText(e);
@@ -23,8 +22,8 @@ let makeActivePhoto = (e) => {
 let changePeopleSayText = (e) => {
     let name = document.getElementById("name");
     let job = document.getElementById("job");
-    name.innerHTML = e.target.getAttribute("name")
-    job.innerHTML = e.target.getAttribute("job")
+    name.innerHTML = e.getAttribute("name")
+    job.innerHTML = e.getAttribute("job")
 }
 
 let playVideo = (e) => {
@@ -35,6 +34,38 @@ let playVideo = (e) => {
     video.classList.remove("hideVideo");
     video.classList.add("showVideo")
     video.play();
+}
+let leftButton = () => {
+    let menu = document.getElementById("peopleSaySlider");
+    let children = menu.children;
+    for (let i=0;i<children.length;i++) {
+        if (children[i].classList.contains("activeImg")) {
+            children[i].classList.remove("activeImg")
+            if (i-1!=0) {
+                children[i-1].classList.add("activeImg")
+                changePeopleSayText(children[i-1]);
+            } else {
+                children[children.length-1].classList.add("activeImg")
+                changePeopleSayText(children[children.length-1]);
+            }
+        }
+    }
+}
+let rightButton = () => {
+    let menu = document.getElementById("peopleSaySlider");
+    let children = menu.children;
+    for (let i=children.length-1;i>=0;i--) {
+        if (children[i].classList.contains("activeImg")) {
+            children[i].classList.remove("activeImg")
+            if ((i+1)!=children.length-1) {
+                changePeopleSayText(children[i+1]);
+                children[i+1].classList.add("activeImg")
+            } else {
+                children[0].classList.add("activeImg")
+                changePeopleSayText(children[0]);
+            }
+        }
+    }
 }
 (function onScroll() {
     window.onscroll = function() {myFunction()};
